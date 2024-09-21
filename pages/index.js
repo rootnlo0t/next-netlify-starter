b8c93a7441bcd99cbeb8bba281f24a6513616d48
rootnlo0t <amanwork12310@gmail.com>
2024-09-20 21:39:59 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,8 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
+        <Header title="Asslamu alaikum" />
+        <p className="description"> root N loot
         </p>
       </main>
 
